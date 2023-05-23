bf6a7dbc9e3378d14cabdefa0249b67523dabc49
SolomonVP <Lum7023@yandex.ru>
2023-05-23 22:44:30 +0300

    init

---
+++ app/src/main/java/com/demo/CreateMessageActivity.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickSendMsg(View view) {
+
+    }
+}
