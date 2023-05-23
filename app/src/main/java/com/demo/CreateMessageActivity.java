35a04072432939f73e3fbfb2c11f2276927628f7
SolomonVP <Lum7023@yandex.ru>
2023-05-23 23:55:10 +0300

    refactor + неявный intent

--- app/src/main/java/com/demo/CreateMessageActivity.java
+++ app/src/main/java/com/demo/CreateMessageActivity.java
@@ -20,8 +20,10 @@ public class CreateMessageActivity extends AppCompatActivity {
 
     public void onClickSendMsg(View view) {
         String msg = editTextMsg.getText().toString();
-        Intent intent = new Intent(this, ReceivedMessageActivity.class);
-        intent.putExtra("msg", msg);
-        startActivity(intent);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, msg);
+        Intent chosenIntent = Intent.createChooser(intent, getString(R.string.chooser_title));
+        startActivity(chosenIntent);
     }
 }
